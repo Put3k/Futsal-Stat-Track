8e07a074fa848d6fad395fb981ff27da0cdf1f92
Put3k <fightepucio@gmail.com>
2023-04-15 14:36:30 +0200

    Add player and match view in admin panel

--- mysite/stat_track/urls.py
+++ mysite/stat_track/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("moderator", views.moderator_panel, name="moderator_panel"),
+    path('player/<int:player_id>/', views.player_stats, name="player_stats"),
+    path('match/<int:match_id>/', views.match_stats, name="match_stats"),
 ]
--- mysite/stat_track/views.py
+++ mysite/stat_track/views.py
@@ -3,7 +3,14 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Home Page")
+    latest_match_list
 
 def moderator_panel(request):
     return HttpResponse("Moderator Panel")
+
+def player_stats(request, player_id):
+    response = "You're looking at the Stats of Player %s."
+    return HttpResponse(response % player_id)
+
+def match_stats(request, match_id):
+    return HttpResponse("You're looking at the stats of Match %s." % match_id)
