8a1c84fb1e82b6a99b6ce955afe62333def084aa
Put3k <fightepucio@gmail.com>
2023-04-23 16:33:22 +0200

    refactor models

--- mysite/stat_track/admin.py
+++ mysite/stat_track/admin.py
@@ -1,47 +1,14 @@
 from django.contrib import admin
 from django import forms
-from .models import Match, Player, Stats
+from .models import Match, MatchDay, Player, Stats
 
-class StatsInline(admin.TabularInline):
-    model = Stats
-    extra = 0
-
-class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('nickname', 'name', 'matches_played_list')
-    inlines = [StatsInline,]
-
-    def matches_played_list(self, obj):
-        matches = obj.get_matches_played()
-        return '. '.join(str(match) for match in matches)
-
-    def change_view(self, request, object_id, form_url='', extra_context=None):
-
-        #get player from database
-        player = Player.objects.get(pk=object_id)
-
-        #create context
-        context = dict(
-            self.admin_site.each_context(request),
-            player=player,
-        )
-
-        return super().change_view(request, object_id, form_url, extra_context=context)
-
+class MatchDayAdmin(admin.ModelAdmin):
+    readonly_fields = ('match_counter', )
 
 class MatchAdmin(admin.ModelAdmin):
-    list_display = ('id', 'date')
+    readonly_fields = ('match_in_matchday',)
 
-class StatsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'match', 'player')
-
-    def save_model(self, request, obj, form, change):
-        #Check if Stats for this player in this day already exists.
-        try:
-            stats = Stats.objects.get(player=obj.player, match=obj.match)
-            self.message_user(request, "Stats for this player in this day already exists.", level='error')
-        except Stats.DoesNotExist:
-            obj.save()
-
-admin.site.register(Player, PlayerAdmin)
+admin.site.register(MatchDay, MatchDayAdmin)
 admin.site.register(Match, MatchAdmin)
-admin.site.register(Stats, StatsAdmin)
+admin.site.register(Player)
+admin.site.register(Stats)
--- mysite/stat_track/models.py
+++ mysite/stat_track/models.py
@@ -4,24 +4,78 @@ from datetime import datetime
 
 class Player(models.Model):
 
-    def get_matches_played(self):        #get all matches that certain player played in
-        return self.match_set.all()
+    first_name = models.CharField(max_length = 16)
+    last_name = models.CharField(max_length = 16)
 
     def __str__(self):
-        return self.nickname
+        return f"{self.first_name} {self.last_name}"
 
-    def get_stats(self):
-        return Stats.objects.filter(player=self)
+    def save(self, *args, **kwargs):
+        if Player.objects.filter(first_name=self.first_name, last_name=self.last_name).exists():
+            player_id = Player.objects.get(first_name=self.first_name, last_name=self.last_name).id
+            raise ValidationError(f"This Player already exists at ID: {player_id}")
+        super(Player, self).save(*args, **kwargs)
+            
 
-    name = models.CharField(max_length=16)
-    nickname = models.CharField(max_length=20)
+class MatchDay(models.Model):
 
-class Match(models.Model):
     date = models.DateTimeField("Date of match")
-    players = models.ManyToManyField(Player, blank=True, default="")
+    match_counter = models.PositiveIntegerField(default=0, )
 
     def __str__(self):
         return f"Matchday {self.date.strftime('%d.%m.%Y')}"
+
+class Match(models.Model):
+
+    TEAM_CHOICES = (
+        ("Blue", "Team Blue"),
+        ("Orange", "Team Orange"),
+        ("Colors", "Team Colors")
+    )
+
+    matchday = models.ForeignKey(MatchDay, on_delete=models.CASCADE, default=None)
+    team_home = models.CharField(choices=TEAM_CHOICES, max_length=20, default="Blue")
+    team_away = models.CharField(choices=TEAM_CHOICES, max_length=20, default="Orange")
+    home_goals = models.IntegerField()
+    away_goals = models.IntegerField()
+    match_in_matchday = models.IntegerField(default = 0)
+
+    @property
+    def score(self):
+        return f"{self.home_goals} - {self.away_goals}"
+
+    @property
+    def result(self):                   #returns 1 for home, 2 for away, 0 for draw
+        if self.home_goals > self.away_goals:
+            return 1
+        elif self.home_goals < self.away_goals:
+            return 2
+        else:
+            return 0
+
+    def clean(self):
+        #check if team_home and team_away are different
+
+        if self.team_home == self.team_away:
+            raise ValidationError("Home team and Away team cannot be the same.")
+
+        if self.home_goals < 0 or self.away_goals < 0:
+            raise ValidationError("Goals scored cannot be negative.")
+
+    def save(self, *args, **kwargs):
+        self.matchday.match_counter += 1
+        self.match_in_matchday = self.matchday.match_counter
+        self.matchday.save()
+        super(Match, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.matchday.match_counter -= 1
+        self.matchday.save()
+        print("Deleted")
+        super(Match, self).delete(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.match_in_matchday}-{self.matchday.date.strftime('%d-%m-%Y')}-{self.team_home}-{self.team_away}"
 
 class Stats(models.Model):
 
@@ -39,12 +93,6 @@ class Stats(models.Model):
     match = models.ForeignKey(Match, on_delete=models.CASCADE)
     team = models.CharField(choices=TEAM_CHOICES, max_length=16)
     goals = models.IntegerField(validators=[positive_validator], default=0)
-    wins = models.IntegerField(validators=[positive_validator], default=0)
-    loses = models.IntegerField(validators=[positive_validator], default=0)
-    draws = models.IntegerField(validators=[positive_validator], default=0)
 
     def __str__(self):
-        return f"ID: {self.id} - {self.player.name} {self.player.nickname} - {self.match}"
-
-    class Meta:
-        verbose_name_plural = "Statistics"
+        return f"ID: {self.id} - {self.match}  - {self.player.first_name} {self.player.last_name}"
