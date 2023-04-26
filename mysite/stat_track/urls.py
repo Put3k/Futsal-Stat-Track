ff4066bb98a5cb9ed0b15831ad1acd456dbf41d4
Put3k <fightepucio@gmail.com>
2023-04-26 19:28:18 +0200

    Add Player view

--- mysite/stat_track/urls.py
+++ mysite/stat_track/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("moderator", views.moderator_panel, name="moderator_panel"),
     path('player/<int:player_id>/', views.player_stats, name="player_stats"),
-    path('match/<int:match_id>/', views.match_stats, name="match_stats"),
+    path('matchday/<int:matchday_id>/', views.matchday, name="matchday"),
 ]
--- mysite/stat_track/views.py
+++ mysite/stat_track/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Match, Player
+from .models import MatchDay, Match, Player, Stat
+from django.db.models.functions import Lower
 
 
 def home(request):
-    latest_match_list = Match.objects.order_by("-date")[:5]
-    context = {"latest_match_list": latest_match_list}
+    latest_match_day_list = MatchDay.objects.order_by("-date")[:5]
+    players_list = Player.objects.all().order_by("last_name")
+    context = {"latest_match_day_list": latest_match_day_list, "players_list": players_list}
     return render(request, "stat_track/home.html", context)
 
 def moderator_panel(request):
@@ -14,6 +16,8 @@ def player_stats(request, player_id):
     player = get_object_or_404(Player, pk=player_id)
     return render(request, "stat_track/player.html", {"player": player})
 
-def match_stats(request, match_id):
-    match = get_object_or_404(Match, pk=match_id)
-    return render(request, "stat_track/match.html", {"match": match})
+def matchday(request, matchday_id):
+    matchday = get_object_or_404(MatchDay, pk=matchday_id)
+    matches_in_matchday_list = Match.objects.filter(matchday=matchday_id)
+    context = {"matches_in_matchday_list": matches_in_matchday_list, "matchday": matchday}
+    return render(request, "stat_track/matchday.html", context)
